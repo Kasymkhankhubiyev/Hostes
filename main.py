f79a02c2b4a10803b930dd552082d69aa59f1d59
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-05-24 20:40:53 +0700

    Commit change

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+import sqlite3 as db
 
 
 if __name__ == '__main__':
-    print('PyCharm')
+    try:
+        dbase = db.connect('Hostel.db')
+        cursor = dbase.cursor()
+
+
+    except db.Error as error:
+        print('Connection error occurred')
+    finally:
+        if dbase:
+            cursor.close()
+            dbase.close()
+            print('Connection with SQL is closed')
