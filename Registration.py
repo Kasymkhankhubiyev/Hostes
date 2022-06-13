07094045b39b2c79e542982d52983ff7eb754e9b
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-13 17:59:24 +0700

    crating calendar

--- Registration.py
+++ Registration.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3 as db
 from tkinter import messagebox
+import calendar
+import datetime
 
 class Registration_Table:
     def __init__(self, window, dbase, notebook):
@@ -21,6 +23,33 @@ class Registration_Table:
             pass
         else:
             pass
+
+    def is_leap_year(self, year):
+        """
+        Проверяем, является ли год високосным
+        :param year: год
+        :return: False/True
+        """
+        if year % 400 == 0 and year % 100 != 100 or year % 400 == 0:
+            return True
+        else: return False
+
+    def draw_calendar(self, current_y, year=None, month=None):
+        self.reg_canvas = tk.Canvas(self.reg_table, width = 300, height=300, bg='green')
+        tk.Label(self.reg_canvas, text='month', font=('Arial', 10), background='blue').place(x=1, y=1)
+        reg_calendar = calendar.TextCalendar()
+        today_date = datetime.datetime.date(datetime.datetime.now())
+        #print(reg_calendar.pryear(today_date.year))
+        month_cal = reg_calendar.formatmonth(today_date.year, today_date.month, w=0, l=0)
+        print(month_cal)
+
+        lines = month_cal.split('  ')
+        print(lines)
+        days = month_cal.split()
+        print(days)
+
+
+        self.reg_canvas.place(x=10, y=current_y)
 
 
     def draw_reg_table(self):
@@ -51,11 +80,17 @@ class Registration_Table:
         self.secname_entry.place(x=120, y=current_y)
         current_y += 60
 
+        self.draw_calendar(current_y)
+
         # self.reg_button = tk.Button(self.window, command=self.draw_reg_table, text='Регистрация', font=('Arial', 17))
         # self.reg_button.grid(row=0, column=0)
 
 
     def check_iin(self):
+        """
+        проверяем наличие указанного ИИН в базе
+        :return:
+        """
         if self.iin_entry.get() != '' and len(self.iin_entry.get()) == 12:
             num = self.iin_entry.get()
             if self.is_integer(num):
@@ -66,6 +101,11 @@ class Registration_Table:
             messagebox.showerror(title='Ошибка ввода.', message=f'Неверно введен ИИН = {self.iin_entry.get()}, \nИИН должен состоять из 12 цифр')
 
     def is_integer(self, string):
+        """
+        Проверяем, составлена ли строка исключительно из цифр.
+        :param string: строка с 12-тью цифрами
+        :return: False or True
+        """
         for i in string:
             if i not in '01234567890':
                 return False
